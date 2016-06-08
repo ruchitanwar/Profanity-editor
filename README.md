6fba9b3be218f4ca43464c4193c852c2997fbe52
Ruchi Tanwar <ruchitanwar271@gmail.com>
2016-06-08 21:17:35 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Take-a-break
-# Turtle
-# Profanity-editor
+This program checks for curse word and returns "Profanity alert" if it detects a curse word.
+movies_quotes.txt is the input file which is checked for curse words.
+check_profanity.py checks the input file.
---
+++ check_profanity.py
@@ -0,0 +1,19 @@
+import urllib
+def read_text():
+	quotes = open("/home/ruchi/Desktop/udacity/profanity_editor/movies_quotes.txt")
+	contents_of_file = quotes.read()
+	#print(contents_of_file)
+	quotes.close()
+	check_profanity(contents_of_file)
+def check_profanity(text_to_check):
+	connection = urllib.urlopen("http://www.wdylike.appspot.com/?q="+text_to_check)
+	output = connection.read()
+	#print(output)
+	connection.close()
+	if "true" in output:
+		print("Profanity Alert!")
+	elif "false" in output:
+		print("This document has no curse words.")
+	else:
+		print("Couldnt scan the complete document")
+read_text()
